ce6b894395648b4de011002f4cb9be72d22a4a9b
Vitor S. do Nascimento <vsantos.nasc@gmail.com>
2025-03-10 21:04:57 -0300

    Update documentation url

--- project/urls.py
+++ project/urls.py
@@ -1,4 +1,4 @@
-# https://docs.djangoproject.com/en/4.2/topics/http/urls/
+# https://docs.djangoproject.com/en/5.1/topics/http/urls/
 
 from django.contrib import admin
 from django.urls import path
